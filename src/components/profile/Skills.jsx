546989cddc71c1b91deca53b51a22563d06e0ef6
Danish Shakeel <danishshakeel54@gmail.com>
2022-01-27 15:32:32 +0530

    Add skills section

--- src/components/profile/Skills.jsx
+++ src/components/profile/Skills.jsx
@@ -1,17 +1,36 @@
 import { Flex, Badge, Heading } from "@chakra-ui/react"
+import { skillsList } from "./_data";
 
 export const SkillBadges = () => {
+	const colorSchemes = ['red', 'teal', 'yellow', 'green', 'purple'];
+
+	Array.prototype.random = function () {
+		return this[Math.floor((Math.random()*this.length))];
+	  }
+
 	return (
 		<>
-		<Heading mb={8}>Skills</Heading>
-		<Heading as="h4" fontSize="lg" fontWeight="600" mb={2}>
-			Technical
-		</Heading>
-		<Flex wrap={'wrap'}>
-			<Badge mr={2}>
-				HTML
-			</Badge>
-		</Flex>
+		<Heading mb={8} id="skills-section">Skills</Heading>
+		{skillsList.map((skillCat, index) => {
+			return (
+				<>
+				<Heading as="h4" fontSize="lg" fontWeight="600" my={4}>
+					{skillCat.name}
+				</Heading>
+				<Flex wrap={'wrap'} direction={'row'}>
+				{skillCat.skills.map((skill, index) => {
+					return (
+						
+						<Badge mr={2} mb={1} colorScheme={colorSchemes.random()}>
+							{skill.name}
+						</Badge>
+					)
+				})}
+				</Flex>
+
+				</>
+			)
+		})}
 		</>
 	);
 }
--- src/components/profile/_data.js
+++ src/components/profile/_data.js
@@ -1,6 +1,5 @@
 export const workExperience = [
     {
-      id: '1',
       company: 'rtCamp',
       role: 'Systems Engineer',
       startDate: '07/2021',
@@ -8,7 +7,6 @@ export const workExperience = [
       isSpecialType: false,
     },
     {
-      id: '2',
       company: 'MightyIQ Inc.',
       role: 'IT Consultant',
       startDate: '12/2021',
@@ -16,7 +14,6 @@ export const workExperience = [
       isSpecialType: 'Freelance',
     },
     {
-      id: '3',
       company: 'Lead Developer',
       role: 'All Things Kashmir',
       startDate: '06/2020',
@@ -24,7 +21,6 @@ export const workExperience = [
       isSpecialType: 'Freelance',
     },
     {
-      id: '4',
       company: 'Data Science Researcher',
       role: 'SKIMS',
       startDate: '04/2020',
@@ -32,7 +28,6 @@ export const workExperience = [
       isSpecialType: 'Internship',
     },
     {
-      id: '5',
       company: 'Full Stack Developer',
       role: 'EzeSkool',
       startDate: '06/2019',
@@ -43,7 +38,6 @@ export const workExperience = [
 
 export const educationExperience = [
     {
-      id: '1',
       institution: 'Chandigarh University',
       degree: 'BE (Hons.)',
       major: 'Computer Science Engineering',
@@ -51,12 +45,139 @@ export const educationExperience = [
       endDate: '2022',
     },
     {
-      id: '2',
-      institution: 'Kendriya Vidyalaya',
+      institution: 'Kendriy',
       degree: 'Intermediate',
       major: 'Physics, Chemistry, Mathematics',
       startDate: '2017',
       endDate: '2018',
     },
   ]
-  
+
+export const skillsList = [
+	{
+		name: 'Industry Knowledge',
+		skills: [
+			{
+	
+				name: 'Web Development',
+			},
+			{
+	
+				name: 'Full Stack Development',
+			},
+			{
+	
+				name: 'Systems Engineering',
+			},
+			{
+	
+				name: 'Cloud and Servers',
+			},
+			{
+	
+				name: 'IT Operations & Consulting',
+			},
+			{
+	
+				name: 'Machine Learning',
+			},
+			{
+	
+				name: 'Research',
+			},
+			{
+	
+				name: 'Applied Quantum Computing',
+			},
+		],
+	},
+	{
+		name: 'Tools & Technologies',
+		skills: [
+			{
+				name: 'JavaScript',
+			},
+			{
+				name: 'PHP',
+			},
+			{
+				name: 'React',
+			},
+			{
+				name: 'Python',
+			},
+			{
+				name: 'Git',
+			},
+			{
+				name: 'Shell Scipting',
+			},
+			{
+	
+				name: 'MySQL',
+			},
+			{
+	
+				name: 'HTML',
+			},
+			{
+				name: 'CSS',
+			},
+			{
+	
+				name: 'WordPress',
+			},
+			{
+				name: 'Docker',
+			},
+			{
+				name: 'Easy Engine',
+			},
+			{
+
+				name: 'Chakra',
+			},
+			{
+				name: 'Nginx',
+			},
+			{
+				name: 'Django',
+			},
+			{
+				name: 'Flask',
+			},
+			{
+				name: 'Qiskit',
+			},
+			{
+				name: 'Ocean SDK',
+			},
+			{
+				name: 'Three.js',
+			},
+			{
+				name: 'Abode Illustrator',
+			},
+			{
+				name: 'Googling',
+			},
+		],
+	},
+	{
+		name: 'Interpersonal Skills',
+		skills: [
+			{
+				name: 'Communication',
+			},
+			{
+				name: 'Problem Solving',
+			},
+			{
+				name: 'Conflict Resolution',
+			},
+			{
+				name: 'Creativity',
+			},
+		],
+	},
+]
