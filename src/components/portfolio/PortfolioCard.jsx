578a086d8de13ebbc54fac4936d68101792b022c
Danish Shakeel <danishshakeel54@gmail.com>
2022-01-31 11:32:25 +0530

    Remove box shadow

--- src/components/portfolio/PortfolioCard.jsx
+++ src/components/portfolio/PortfolioCard.jsx
@@ -36,7 +36,7 @@ import {
 		})}
 		{...rootProps}
 	  >
-		<Box position="relative" overflow={"hidden"} borderRadius={"lg"} boxShadow={'md'}>
+		<Box position="relative" overflow={"hidden"} borderRadius={"lg"}>
 		  <AspectRatio ratio={4 / 3}>
 			<Image
 			  src={imageUrl}
